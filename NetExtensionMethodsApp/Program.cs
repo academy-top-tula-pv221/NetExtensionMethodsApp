d4462cf522d4e56298987a7b86c59fe8329d9e79
maxefimov <efimov.academy.top@gmail.com>
2023-02-20 21:18:54 +0300

    Добавьте файлы проекта.

---
+++ NetExtensionMethodsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NetExtensionMethodsApp
+{
+    public static class StringExtension
+    {
+        public static bool IsChar(this string str, char ch)
+        {
+            foreach(char c in str)
+                if(c == ch) return true;
+            return false;
+        }
+        public static int CountChar(this string str, char ch)
+        {
+            int count = 0;
+            foreach (char c in str)
+                if (c == ch) count++;
+            return count;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello world";
+            char ch = 'w';
+            bool f = str.IsChar(ch);
+
+            Console.WriteLine(f);
+            Console.WriteLine(str.CountChar('l'));
+        }
+    }
+}
